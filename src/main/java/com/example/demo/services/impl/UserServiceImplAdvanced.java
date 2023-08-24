9b1617af12b360120308dbcd67c8a016f45c4a36
AntonLiutyi <anton.liutyi@cpcs.ws>
2023-08-24 13:13:03 +0300

    Add second implementation of UserService for future purposes.

---
+++ src/main/java/com/example/demo/services/impl/UserServiceImplAdvanced.java
@@ -0,0 +1,49 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplAdvanced implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    public List<User> listUsers(boolean ascending) {
+        if (ascending) {
+            return listUsers();
+        } else {
+            return userRepository.findAllByOrderByIdDesc();
+        }
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void saveUsers(User... users) {
+        userRepository.saveAll(Arrays.asList(users));
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void deleteUsers() {
+        userRepository.deleteAll();
+    }
+}
