c3270bc48cc91d89303917fb43b8e3151258fe73
AntonLiutyi <anton.liutyi@cpcs.ws>
2023-08-25 15:14:06 +0300

    Improve caching mechanism, convert stored values to JSON format.

---
+++ src/main/java/com/example/demo/configs/CacheConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.configs;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisSerializationContext.SerializationPair<Object> jsonSerializer =
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(jsonSerializer);
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
--- src/main/java/com/example/demo/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.demo.repositories.UserRepository;
 import com.example.demo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -17,19 +18,19 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    @Cacheable("users")
+    @Cacheable(value = "users", key = "'all'")
     public List<User> listUsers() {
         return userRepository.findAll();
     }
 
     @Override
-    @CacheEvict(value = "users", allEntries = true)
-    public void saveUser(User user) {
-        userRepository.save(user);
+    @CachePut(value = "users", key = "#user.id")
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 
     @Override
-    @CacheEvict(value = "users", allEntries = true)
+    @CacheEvict(value = "users", key = "#id")
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
